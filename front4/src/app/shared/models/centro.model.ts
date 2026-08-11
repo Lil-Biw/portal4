c70ff01aa00115d0e348bc85c409f4418363e27e
Lil-Biw <andresgalaz10@hotmail.com>
2026-08-10 20:51:25 -0400

    feat(front4): agregar campo foto al modelo CentroCosto

--- front4/src/app/shared/models/centro.model.ts
+++ front4/src/app/shared/models/centro.model.ts
@@ -11,6 +11,7 @@ export interface CentroCosto {
   ubicacion_latitud?: number;
   ubicacion_longitud?: number;
   activo: boolean;
+  foto?: { tipo_mime: string; nombre: string };
   score_smartclarity?: number[];
   creado_en?: string;
   actualizado_en?: string;
